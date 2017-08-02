7c062173338445003ae63eb1fb909df9764cb645
doukang <doukah@foxmail.com>
2017-08-02 23:45:26 +0800

    完善页面和controller

--- spring-base/src/org/doukang/springbase/web/LoginController.java
+++ spring-base/src/org/doukang/springbase/web/LoginController.java
@@ -1,6 +1,47 @@
 package org.doukang.springbase.web;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.doukang.springbase.domain.User;
+import org.doukang.springbase.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/login/")
 public class LoginController
 {
+    @Autowired
+    private UserService userService;
     
+    @RequestMapping("index.html")
+    public String loginPage()
+    {
+        return "login";
+    }
+    
+    @RequestMapping("check.html")
+    public ModelAndView loginCheck(HttpServletRequest request, User user)
+    {
+        boolean isValidUser =
+                userService.hasMatchUser(user.getUserName(), user.getPassword());
+        if (!isValidUser)
+        {
+            return new ModelAndView("login", "error", "用户名或密码错误！");
+        }
+        else
+        {
+            user = userService.getUserByUserName(user.getUserName());
+            user.setLastVisit(new Date());
+            user.setLastIP(request.getRemoteAddr());
+            userService.loginSuccess(user);
+            request.getSession().setAttribute("user", user);
+            return new ModelAndView("main");
+        }
+        
+    }
 }
